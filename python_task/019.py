9a8f0c9f7b86a15b2d8b73f639327c0933a37e3e
xingzhaonian <707547168@qq.com>
2022-11-04 18:04:39 +0800

    019代码修改

--- python_task/019.py
+++ python_task/019.py
@@ -7,11 +7,11 @@ r'''
 答: s = []  这样就创建了一个空的列表
 
 2. 你知道什么是匿名列表吗？
-答: 匿名列表就是只使用一次的列表, 临时使用, 用来存放一些临时数据
+答: 匿名列表就是创建了一个列表 [], 没有将其进行赋值, 由于没有名字, 也无法进行访问, 比较鸡肋
 
 3. 如果有一个列表 list1，有两种方法可以获取到该列表的最后一个元素，你知道分别是什么吗？
 s = [1,2,3,4,5,6]
-答: s[len(s - 1)];  s[-1] 两种
+答: s[len(s) - 1];  s[-1] 两种
 
 4. 请问下面代码打印的结果是什么？
 [1, 2, 3, 4, 5][:3]
@@ -23,7 +23,7 @@ s = [1,2,3,4,5,6]
 
 6. 请问下面代码打印的结果是什么？
 [5, "上", 4, "山", 3, "打", 2, "老", 1, "虎"][-2::-2]
-答: 含义是反向取列表中 倒数第二个至第零个的所有元素, 步长为2, 每隔一个取一个
+答: 含义是反向取列表中 倒数第二个至第零个的所有元素, 步长为2, 每隔一个取一个  即：1, 2, 3, 4, 5
 
 7. 下面有两列表，请问如何将 list2 列表中的全部元素，添加到 list1 列表中第 2 和第 3 个元素的中间
 list1 = [1, 2, 8, 9]
@@ -34,38 +34,58 @@ for i in list2:
     list1.insert(count, i)
     count += 1
 
+list1 = list1[:2] + list2 + list1[2:]
+
 ----动动手----
 0. 给定一个整数列表 nums 和一个目标值 target, 请在该数组中找出和为目标值的两个元素，并将它们的数组下标值打印出来
-比如给定的列表 nums = [2, 7, 11, 15]，目标值 target = 9，那么由于 nums[0] + nums[1] = 2 + 7 = 9，所以打印结果是：[0, 1]
+比如给定的列表 nums = [2, 7, 11, 15]，目标值 target = 9，那么由于 nums[0] + nums[1] = 2 + 7 = 9，所以打印结果是：[0, 1]'''
 
-'''
-num = []
+
+num_list = []
+
 while True:
-    nums = input('请输入一个整数(输入stop结束)')
-    if nums == 'stop':
+    _ = input('请录入一个整数(输入stop结束)')
+    if _ == ('stop'):
         break
-    else:
-        nums = int(nums)
-        num.append(nums)
-target = int(input('请输入目标整数'))
-num_1 = num
-find = False
-for i in num:
-    for k in num_1:
-        if i == k:
-            continue
-        if i + k == target:
-            find = True
-            print([num.index(i), num_1.index(k)])
-            break
-    if find:
-        break
-if not find:
+    _ = int(_)
+    list_length = len(num_list)
+    num_list.insert(list_length, _)
+
+list_length = len(num_list)
+print(num_list)
+
+isfind = False
+target = int(input('输入目标数字'))
+for i in range(list_length):
+    for k in range(i + 1, list_length):
+        if num_list[i] + num_list[k] == target:
+            isfind = True
+            print([i, k])
+if not isfind:
     print('未找到')
 
+
+
+
 r'''
-2. 最后，回忆前面我们学习过的 random 模块，生成一个由 10000 个整数（范围是 1 ~ 65535）构成的随机列表，目标值 target 由用户输入
+2. 最后，回忆前面我们学习过的 random 模块，生成一个由 10000 个整数（范围是 1 ~ 65535）构成的随机列表，目标值 target 由用户输入'''
 
-'''
+
 import random
+count = 10000
+random_list = []
+is_find_rand_num = False
+random_target = int(input('输入随机目标数字'))
+while count:
+    random_num = random.randint(1, 65535)
+    random_list.append(random_num)
+    count -= 1
 
+random_list_light = len(random_list)
+for i in range(random_list_light):
+    for k in range(i + 1, random_list_light):
+        if random_list[i] + random_list[k] == random_target:
+            print([i, ':', random_list[i], '====', k, ':', random_list[k]])
+            is_find_rand_num = True
+if not is_find_rand_num:
+    print('未找到随机目标数字')
