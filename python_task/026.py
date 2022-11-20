7a04b835df06bf361e18ccf4509eb030a76c5d5d
xingzhaonian <707547168@qq.com>
2022-11-20 11:24:09 +0800

    列表推导式

--- python_task/026.py
+++ python_task/026.py
@@ -76,6 +76,10 @@ matrix = [[1, 2, 3, 4],
           [5, 6, 7, 8],
           [9, 10, 11, 12]]
 
+Matrix = [[k[i] for k in matrix] for i in range(len(matrix[0]))]
+
+'''传统循环'''
+
 row_range = len(matrix)
 clo_range = len(matrix[0])
 output_result = [[]*row_range  for i in range(clo_range)]
