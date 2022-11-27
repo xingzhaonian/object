8fc1d8334f145e33965f96e52bb3de5f8a5cc102
xingzhaonian <707547168@qq.com>
2022-11-27 11:06:12 +0800

    bug fix

--- python_task/028.py
+++ python_task/028.py
@@ -11,7 +11,7 @@ r'''
 
 3. 请问下面代码执行的结果是？
 >>> "-520".center(6, "0").zfill(10)
-'00000-5200'.zfill(10) 
+答: '00000-5200'
 
 4. 请问下面代码执行的结果是？
 >>> "I love FishC".swapcase()[::-1]
@@ -43,7 +43,7 @@ r'''
 整理后："AABaAbCc" -> "AABbCc" -> "AACc" -> "AA"
 
 请按要求整理好字符串,并将结果打印到屏幕上。
-提示（小甲鱼不想限制大家的思路,如果想看的话,请使用鼠标刮开叭~）：将字符串逐个读取,整理后放到一个列表中,最后将列表中的元素挨个打印即可。
+提示(小甲鱼不想限制大家的思路,如果想看的话,请使用鼠标刮开叭~)：将字符串逐个读取,整理后放到一个列表中,最后将列表中的元素挨个打印即可。
 
 '''
 temp_list = []
@@ -53,9 +53,31 @@ for i in range(string_lenght):
     if not temp_list:
         temp_list.append(string[i])
         continue
-    if string_lenght[i - 1 ].islower():
-        pass
-        
+    if (temp_list[-1].upper() == string[i].upper()) and  (temp_list[-1].lower() == string[i].lower()) and (temp_list[-1] != string[i]):
+        temp_list.pop()
+    else:
+        temp_list.append(string[i])
+for i in temp_list:
+    print(i, end = '')
+print('')
 
-print(temp_list)
+r'''
+1. 给定的字符串 s 是按照如下规则存放的：它的偶数下标为小写英文字母,奇数下标为正整数。
+题目要求：编写代码,将奇数下标的数字转换为相对于上一个字母偏移后的字母
+比如 s = "a1b2c3" 转换后的结果是 "abbdcf"(a1 -> ab,b2 -> bd,c3 -> cf);s = "x7y8z9" 转换后的结果是 "xeygzi"(遇到最后字母 z ,则从 a 继续计算偏移)
+提示(小甲鱼不想限制大家的思路,如果想看的话,请使用鼠标刮开叭~)：你可能会用到 chr() 和 ord() 这两个函数。
 
+'''
+s = input("请按规则输入一个字符串：")
+   
+length = len(s)
+res = []
+# 获取字母 a 的编码值
+base = ord('a')
+   
+# 从第一个元素开始，每次迭代跳过一个元素
+for i in range(0, length, 2):
+    # ord(s[i]) - base 操作得到一个字母的偏移值，比如 b 就是 1
+    # 跟 26 求余数的作用是防止溢出，循环计算偏移
+    shift = chr((ord(s[i]) - base + int(s[i+1])) % 26 + base)
+    print(s[i]+shift, end="")
