9bf16925cf1b86193b08c410c89fbfc8c0a83f0c
xingzhaonian <707547168@qq.com>
2023-03-06 17:59:29 +0800

    Update 032.py
    压缩&解压缩

--- python_task/032.py
+++ python_task/032.py
@@ -1,15 +1,36 @@
-text = 'ilovefishccccccdwdagaaaaaaFFiiiisshCjCCCCC'
-result_list = []
-for i in text:
-    if text.count(i) >= 3:
-        if i in result_list:
+# 压缩
+compress_text = 'iiiiiilovefssshccccccdwdaaaaaaagFFhCCCCCCj'
+compress_result_list = []
+for i in compress_text:
+    if compress_text.count(i) >= 3:
+        if i in compress_result_list:
             continue
         else:
-            result_list.append(str(i))
-            result_list.append(str(text.count(i)))
+            compress_result_list.append(str(i))
+            compress_result_list.append(str(compress_text.count(i)))
     else:
-        result_list.append(str(i))
-result_txt = ''.join(result_list)
-text_num = len(text)
-result_txt_num = len(result_txt)
-print(f'压缩前为:{text};  压缩后为:{result_txt},   压缩率为:{result_txt_num / text_num * 100:.2f} %')
+        compress_result_list.append(str(i))
+compress_result_txt = ''.join(compress_result_list)
+text_num = len(compress_text)
+result_txt_num = len(compress_result_txt)
+print(f'压缩前为:{compress_text};  压缩后为:{compress_result_txt},   压缩率为:{result_txt_num / text_num * 100:.2f} %')
+
+
+# 解压缩
+decompression_text = 'i6lovefs3hc6dwda7gFFhC6j'
+decompression_result_list = []
+index_count = 0
+for i in decompression_text:
+    if i.isdecimal():
+        for k in range(int(i) - 1):
+            x = decompression_text.index(i, index_count - 1 , len(decompression_text)) - 1
+            y = decompression_text.index(i, index_count - 1 , len(decompression_text))
+            index_text = decompression_text[decompression_text.index(i, index_count - 1 , len(decompression_text)) - 1 :\
+                                             decompression_text.index(i, index_count - 1 , len(decompression_text))]
+            decompression_result_list.append(index_text)
+    else:
+        decompression_result_list.append(i)
+    index_count += 1
+
+decompression_result_text = ''.join(decompression_result_list)
+print(decompression_result_text)
