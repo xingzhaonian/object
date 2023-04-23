eceb3dfdeb1f0642b97a4b1005fcdae13f46aa02
xingzhaonian <707547168@qq.com>
2023-04-23 15:04:17 +0800

    fix

--- Python_Project/down_load_music.py
+++ Python_Project/down_load_music.py
@@ -1,11 +1,11 @@
 import requests
-url = 'https://dl.stream.qqmusic.qq.com/C400000y39oA2VspA4.m4a?guid=5703167320&vkey=2B8A3DEC13E7006E8B1DD032D480F38DA3B155B6B4939E9D70B8613C0EF0C3AD4C01FD4442D37199BC75A06760CA14D06FF3D6A0BDF5AE4A&uin=&fromtag=120032'
+url = 'https://dl.stream.qqmusic.qq.com/C400000y39oA2VspA4.m4a?guid=8158688256&vkey=8616A5F3E5201A5554FB1DE1E8ABCA782FCF62E5FFA13B598A302BB30D83BFC939D6FCB882EE7CCB31857024654EF8230E3EE5E1EAC8A9F5&uin=&fromtag=120032'
 def down_load_music(url):
     headers_info = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44'}
     res = requests.get(url, headers = headers_info, timeout = 30)
     print('请求结果', type(res), res)
     data = res.content
     print(data, '类型:', type(data))
-    music = open('D:\\Project\\music\\蔚蓝海岸.mp3', 'wb')
+    music = open('D:\\Project\\music\\可能-程响.mp3', 'wb')
     music.write(data)
 down_load_music(url)
