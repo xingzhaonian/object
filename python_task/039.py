b85ef4224e7db867d4ec96c9e48d76631b172898
xingzhaonian <707547168@qq.com>
2023-04-25 15:12:48 +0800

    Update 039.py

--- python_task/039.py
+++ python_task/039.py
@@ -46,19 +46,18 @@ set("FishC.com.cn").issuperset('FishC')
 
 import random
 import timeit
+def init_list():
+    haystack = [random.randint(1, 10000000) for i in range(10000000)]
+    needles = [random.randint(1, 1000) for i in range(1000)]
 
-haystack = [random.randint(1, 10000000) for i in range(10000000)]
-needles = [random.randint(1, 1000) for i in range(1000)]
-
-# 请在此处添加一行代码，使得查找过程的执行效率提高 10000 倍以上。
-haystack = set(haystack)
-
+    # 请在此处添加一行代码，使得查找过程的执行效率提高 10000 倍以上。
+    return needles, set(haystack)
+init_list_result = init_list()
 def find():
     found = 0
-    for each in needles:
-        if each in haystack:
+    for each in init_list_result[0]:
+        if each in init_list_result[1]:
             found += 1
-
     print(f"一共找到{found}个匹配。")
 
 t = timeit.timeit("find()", setup="from __main__ import find", number=1)
