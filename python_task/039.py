845ce2a7030b6f2b949760eb569363d2a803e039
xingzhaonian <707547168@qq.com>
2023-05-04 14:50:34 +0800

    Update 039.py

--- python_task/039.py
+++ python_task/039.py
@@ -49,10 +49,10 @@ import timeit
 def init_list():
     haystack = [random.randint(1, 10000000) for i in range(10000000)]
     needles = [random.randint(1, 1000) for i in range(1000)]
-
-    # 请在此处添加一行代码，使得查找过程的执行效率提高 10000 倍以上。
     return needles, set(haystack)
+
 init_list_result = init_list()
+
 def find():
     found = 0
     for each in init_list_result[0]:
