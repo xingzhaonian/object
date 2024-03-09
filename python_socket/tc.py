3fb1d853e9499d137854eee16ad4bb3dfca2b9ca
xingzhaonian <707547168@qq.com>
2024-03-09 13:18:35 +0800

    客户端发送的消息增加判空处理

--- python_socket/tc.py
+++ python_socket/tc.py
@@ -1,30 +1,34 @@
-# ==== TCP 客户端程序 client.py ====
+# ===== 我是客户端 client.py =====
 
 import socket
-# 初始化IP地址
-IP = '127.0.0.1'
-SERVER_PORT = 50000
-BUFLEN = 512
-print('我是客户端')
-# 实例化 socket对象, 指明协议
-dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('客户端准备进行连接服务器。。。')
-dataSocket.connect((IP, SERVER_PORT))
-print('客户端连接服务器成功, 你可以发送消息了')
+
+ip = '192.168.1.3'
+port = 50000
+buflen = 1024
+
+socketObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketObject.connect((ip, port))
 
 while True:
-    toSend = input('>>')
-    if toSend == 'exit':
-        break
-    # 发送进行编码后的消息
-    dataSocket.send(toSend.encode())
+    send_data = input('请发送消息>>>')
 
-    # 等待接收服务端的消息
-    recved = dataSocket.recv(BUFLEN)
-    # 返回为空bytes, 表示对方关闭了连接
-    if not recved:
-        break
-    print(f'服务器返回的消息为{recved.decode()}')
-dataSocket.close()
+    if not send_data:
+        print('发送消息为空, 请重新输入')
+        continue
 
+    if send_data == 'exit':
+        print('客户端中断连接')
+        break
+
+    print(f'向服务器发送消息, 内容为{send_data}')
+    send_data = send_data.encode('utf-8')
+    print(f'编码后为{send_data}')
+    socketObject.send(send_data)
+    print('向服务器发送成功, 等待服务器返回消息>>>')
+    print('===================*********************=====================')
+    receive_data  = socketObject.recv(buflen)
+    print(f'接收到服务器返回消息, 内容为{receive_data}')
+    print(f"进行解码, 解码后的内容为{receive_data.decode('utf-8')}")
+
+socketObject.close()
 
--- python_socket/ts.py
+++ python_socket/ts.py
@@ -1,38 +1,37 @@
-# ==== TCP 服务器程序 server.py =====
+# ===== 我是服务器 server.py =====
 
 import socket
-# 初始化IP
-IP = '127.0.0.1'
-PORT = 50000
-BUFLEN = 512
-print('我是服务器')
-listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listenSocket.bind((IP, PORT))
 
-listenSocket.listen(5)
-print(f'服务端启动成功，在{PORT}端口等待客户端连接...')
-dataSocket, addr = listenSocket.accept()
-print('接受了一个客户端连接, IP为:', addr[0], '端口为:',addr[1])
+ip = '192.168.1.3'
+port = 50000
+buflen = 1024
+
+socketObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketObject.bind((ip, port))
+socketObject.listen(5)
+print(f'服务器启动成功, 在{port}端口等待进行连接...')
+connetObject, addr = socketObject.accept()
+print(f'连接对象:{connetObject}, ip地址:{addr[0]}, 端口:{addr[1]}')
 
 while True:
-    recved = dataSocket.recv(BUFLEN)
-
-    # 如果对方返回空的bytes, 表示对方关闭了连接, 退出循环, 结束消息收发
-    if not recved:
+    receive_data = connetObject.recv(buflen)
+    print(f'客户端发来了消息, 内容是{receive_data}')
+    if not receive_data:
         break
-    # 读取的字节数据是bytes类型, 需要解码
-    info = recved.decode()
-    print(f'客户端发来的消息：{recved}, 解析后为{info}')
-    send_info = recved + b'LOL'
+    receive_data = receive_data.decode('utf-8')
+    print(f'解码后为{receive_data}')
 
-    # 发送的数据类型必须是bytes, 所以要进行编码
-    print(f'向客户端返回消息：{send_info}', '解析后为', send_info.decode())
-    
-    dataSocket.send(send_info)
 
-# 服务端也调用close() 关闭socket
-dataSocket.close()
-listenSocket.close()
+    receive_data = receive_data.upper()
+    print(f'向客户端返回消息, 内容为{receive_data}')
+    send_data = receive_data.encode('utf-8')
+    print(f'需要进行编码, 编码后内容为{send_data}')
+    connetObject.send(send_data)
+    print('返回消息完成, 等待再次接收消息中>>>')
+
+connetObject.close()
+socketObject.close()
+
 
 
 
