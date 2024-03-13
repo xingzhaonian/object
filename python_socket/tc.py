58f8cd9d19dd1e7f008c1c8ed7ed471d3c132d16
xingzhaonian <707547168@qq.com>
2024-03-14 00:06:37 +0800

    封装客户端

--- python_socket/tc.py
+++ python_socket/tc.py
@@ -2,38 +2,55 @@
 
 # 导入 socket模块
 import socket
+import time 
 
-# 初始化ip, 端口, 每次传输最大字节数
-ip = '127.0.0.1'
-port = 50000
-ClientBufLen = 1024
+# 管理客户端连接和相关功能
+class Client(object):
+    def __init__(self):
+        self.ip = '127.0.0.1'
+        self.port = 50000
+        self.reconnectTimes = 10
+        self.connectObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.connectObject.connect((self.ip, self.port))
+            print('连接成功')
+        except:
+            print('连接失败, 准备进行重连>>>')
+            while self.reconnectTimes:
+                try:
+                    self.connectObject.connect((self.ip, self.port))
+                    print('连接成功')
+                    break
+                except:
+                    self.reconnectTimes -= 1
+                    print(f'重连剩余{self.reconnectTimes}次')
+                    time.sleep(2)
 
-# 指定协议, 创建连接对象
-client_ConnectObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def sendMessage(self):
+        self.message = input('请输入要发送的消息>>>')
+        while not self.message:
+            self.message = input('输入为空, 重新输入>>>')
+        if self.message == 'exit':
+            self.connectObject.close()
+            return '404'
+        self.connectObject.send(self.message.encode(encoding='utf-8'))
 
-# 向服务器发起连接请求进行连接
-client_ConnectObject.connect((ip, port))
-print('连接成功')
+    def reviceData(self):
+        print('开始接收')
+        self.rawData = self.connectObject.recv(1024)
+        print('进行转码')
+        self.resultData =  self.rawData.decode(encoding='utf-8')
+        return self.resultData
 
-while True:
-    # 向服务器发送消息
-    client_SendData = input('请发送消息>>>')
-
-    # 判空处理
-    if not client_SendData:
-        continue
-
-    # 输入 'exit' 退出连接
-    if client_SendData == 'exit':
-        break
-
-    # 将要发送的数据进行编码
-    after_TranscodeClientSendData = client_SendData.encode('utf-8')
-
-    # 发送转码后的数据
-    client_ConnectObject.send(after_TranscodeClientSendData)
-    print('发送成功, 等待服务器返回消息')
-    
-    # 接收服务器返回的消息
-    client_ReceiveData = client_ConnectObject.recv(ClientBufLen)
-    print(f"接收服务器返回消息成功, 内容为:{client_ReceiveData},  编码后内容为:{client_ReceiveData.decode('utf-8')}")
+    def run(self):
+        self.status = None
+        while True:
+            self.status = self.sendMessage()
+            if self.status == '404':
+                print('客户端关闭连接 >>>')
+                break
+            self.data = self.reviceData()
+            print(f'接收到服务器返回消息, 内容为{self.data}')
+               
+c = Client()
+c.run()
