f383b4d7daebdbaacb198833347424a6e0cdffc8
xingzhaonian <707547168@qq.com>
2024-03-17 19:49:46 +0800

    上传\下载 方法封装

--- python_socket/cmd文件客户端.py
+++ python_socket/cmd文件客户端.py
@@ -1,59 +1,68 @@
 # cmd文件 客户端
+
 import socket
 import json
 import pathlib
 
-ip = '127.0.0.1'
-port = 5060
+class ClientSocket(object):
+    def __init__(self):
+        self.ip = '127.0.0.1'
+        self.port = 5060
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((ip, port))
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.ip, self.port))
 
-# 传给客户端header信息
-while True:
-    cmd = input('输入指令 >>>')
-    while not cmd:
-        cmd = input('指令为空, 请重新输入')
-    cmd_str_h = len(cmd)
+    # 下载
+    def download(self):
+        while True:
+            self.cmd = input('输入要下载的文件(包含绝对路径) >>>')
+            while not self.cmd:
+                self.cmd = input('输入为空, 请重新输入 >>>')
+            self.cmd_str_h = len(self.cmd)
+            self.cmd_bytes_h = bytes(str(self.cmd_str_h), encoding = 'utf-8').zfill(4)
+            
+            # 发送请求  文件名+绝对路径的请求大小和序列化后的请求
+            self.client.send(self.cmd_bytes_h)
+            self.client.send(self.cmd.encode('utf-8'))
 
-    cmd_bytes_h = bytes(str(cmd_str_h), encoding = 'utf-8').zfill(4)
-    print(f"发送指令的大小给服务器==={int(cmd_bytes_h.decode('utf-8'))}")
-    client.send(cmd_bytes_h)
-
-    print(f"发送序列化后的指令==={cmd.encode('utf-8')}")
-    client.send(cmd.encode('utf-8'))
-
-    # 收取头部信息长度
-    header_data_h = int(client.recv(4).decode('utf-8'))
-    print(f"获取到头部信息长度:==={header_data_h}")
-
-    # 收取json头部数据
-    json_data = client.recv(header_data_h)
-    print(f"获取到json头部数据:==={json_data}")
-    json_tranform_dict = json.loads(json_data)
-    print('文件名===', json_tranform_dict['file_name'])
-    print('文件大小===', json_tranform_dict['file_size'])
-    print('MD5信息===', json_tranform_dict['md5'])
+            # 收取头部信息长度
+            self.header_data_h = int(self.client.recv(4).decode('utf-8'))
 
 
-    # 收取数据大小信息
-    data_h = int(client.recv(8).decode('utf-8'))
-    print(f"收取到数据内容信息的大小:==={data_h}")
+            # 收取json头部数据
+            self.json_data = self.client.recv(self.header_data_h)
+            self.json_tranform_dict = json.loads(self.json_data)
 
-    # 收取数据内容
-    data = bytes()
-    recv_data_h = 0
-    while recv_data_h < data_h:
-        res_data = client.recv(1024)
-        data += res_data
-        recv_data_h += len(res_data)
+            # 获取数据头的相关信息
+            self.header_info_name = self.json_tranform_dict['file_name']
+            self.header_info_file_size = self.json_tranform_dict['file_size']
+            self.header_info_md5 = self.json_tranform_dict['md5']
 
-    path = pathlib.Path('D:/project/object/test_file/download.txt')
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(data.decode('utf-8'))
-        
-    #print(f"服务器返回的数据总体内容为{data.decode(encoding='utf-8')}")
-    print(f'接收到服务返回总体数据的长度为==={len(data)}')
+            # 收取数据大小信息
+            self.data_h = int(self.client.recv(8).decode('utf-8'))
+
+
+            # 收取数据内容
+            self.data = bytes()
+            self.recv_data_h = 0
+            while self.recv_data_h < self.data_h:
+                res_data = self.client.recv(1024)
+                self.data += res_data
+                self.recv_data_h += len(res_data)
+            
+            # 下载服务器传输的数据
+            path = pathlib.Path('D:/project/object/test_file/download.txt')
+            with open(path, 'w', encoding='utf-8') as f:
+                f.write(self.data.decode('utf-8'))                
+            #print(f"服务器返回的数据总体内容为{data.decode(encoding='utf-8')}")
+                
+    # 上传
+    def Upload(self):
+        pass
+                
+if __name__ == '__main__':
+    c = ClientSocket()
+    c.download()
 
 
 
--- python_socket/cmd文件服务器.py
+++ python_socket/cmd文件服务器.py
@@ -5,79 +5,84 @@ import json
 import pathlib
 import hashlib
 
-ip = '127.0.0.1'
-port = 5060
+class serverSocket(object):
+    def __init__(self):
+        self.ip = '127.0.0.1'
+        self.port = 5060
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.ip, self.port))
+        self.server.listen(5)
+        print('服务器启动成功, 等待客户端连接')
+        self.conncet_status = None
+        while True:
+            try:
+                self.connectObject, self.addres = self.server.accept()
+                self.conncet_status = 1
+                if self.conncet_status == 1:
+                    print(f'拿到连接对象, 对象信息:==={self.connectObject}, \n ip:{self.addres[0]}, \n port:{self.addres[1]}')
+                    break
+            except:
+                self.connectObject.close()
+                break
+    def Upload(self):
+        while True:
+            try:
+                self.res_data_size = int(self.connectObject.recv(4).decode(encoding='utf-8'))
+                print(f'客户端发来的消息长度==={self.res_data_size}')
+            except:
+                self.connectObject.close()
+                break
+            self.data = bytes()
+            self.each_size = 0
+            while self.each_size < self.res_data_size:
+                res_data = self.connectObject.recv(1024)
+                self.data += res_data
+                self.each_size += len(res_data)
+            self.cmd_data = self.data.decode('utf-8')
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((ip, port))
-server.listen(5)
-print('服务器启动成功, 等待客户端连接')
+            with open(pathlib.Path(self.cmd_data), 'r', encoding='utf-8')as f:
+                self.data_txt = f.read()
+            #obj = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            #stdout = obj.stdout.read()
+            #3stderr = obj.stderr.read()
+            #result_data_h = len(stdout) +len(stderr)
+            
+            self.header = {
+                'file_name': self.cmd_data.split('\\')[-1],
+                'file_size':pathlib.Path(self.cmd_data).stat()[6],
+                'md5':hashlib.md5(self.data_txt.encode('utf-8')).hexdigest(),
+                'isuse':True
+            }
 
-while True:
-    try:
-        connectObject, addres = server.accept()
-        print(f'拿到连接对象, 对象信息:==={connectObject}, \n ip:{addres[0]}, \n port:{addres[1]}')
-    except:
-        connectObject.close()
-        break
-    while True:
-        try:
-            res_data_size = int(connectObject.recv(4).decode(encoding='utf-8'))
-            print(f'客户端发来的消息长度==={res_data_size}')
-        except:
-            connectObject.close()
-            break
-        data = bytes()
-        each_size = 0
-        while each_size < res_data_size:
-            res_data = connectObject.recv(1024)
-            data += res_data
-            each_size += len(res_data)
-        cmd_data = data.decode('utf-8')
+            self.return_data = self.data_txt.encode('utf-8')
+            self.data_txt_h = len(self.data_txt.encode('utf-8'))
+            self.data_h = bytes(str(self.data_txt_h), encoding='utf-8').zfill(8)
 
-        with open(pathlib.Path(cmd_data), 'r', encoding='utf-8')as f:
-            data_txt = f.read()
-        #obj = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        #stdout = obj.stdout.read()
-        #3stderr = obj.stderr.read()
-        #result_data_h = len(stdout) +len(stderr)
-        
-        header = {
-            'file_name': cmd_data.split('\\')[-1],
-            'file_size':pathlib.Path(cmd_data).stat()[6],
-            'md5':hashlib.md5(data_txt.encode('utf-8')).hexdigest(),
-            'isuse':True
-        }
+            self.heard_json = json.dumps(self.header)
 
-        return_data = data_txt.encode('utf-8')
-        data_txt_h = len(data_txt.encode('utf-8'))
-        data_h = bytes(str(data_txt_h), encoding='utf-8').zfill(8)
+            self.heard_json_info = self.heard_json.encode(encoding = 'utf-8')        
 
-        heard_json = json.dumps(header)
-        print(f'已将dict转为json, 内容为==={heard_json}')
+            hearer_h = bytes(str(len(self.heard_json_info)), encoding = 'utf-8').zfill(4)
 
-        heard_json_info = heard_json.encode(encoding = 'utf-8')        
-        print(f'已将json进行序列化, 内容为==={heard_json_info}')
+            # 发送头部信息长度
+            self.connectObject.send(hearer_h)
 
-        hearer_h = bytes(str(len(heard_json_info)), encoding = 'utf-8').zfill(4)
-        print(f"拿到json序列化后的长度, 也就是头部信息的长度, 长度是==={int(hearer_h.decode(encoding='utf-8'))}")
+            # 发送头部json数据
+            self.connectObject.send(self.heard_json_info)
 
-        # 发送头部信息长度
-        connectObject.send(hearer_h)
-        print(f"已经将头部信息长度发送给客户端, 长度为==={int(hearer_h.decode('utf-8'))}")
+            # 发送数据内容大小
+            self.connectObject.send(self.data_h)
+           
+            #发送内容
+            self.connectObject.send(self.return_data)
 
-        # 发送头部json数据
-        connectObject.send(heard_json_info)
-        print(f'已经将头部json数据发给客户端, 内容为==={heard_json_info}')
 
-        # 发送数据内容大小
-        connectObject.send(data_h)
-        print(f"已经将数据内容大小发给客户端, 长度为==={int(data_h.decode(encoding='utf-8'))}")
+    def download(self):
+        pass
 
-        #发送内容
-        connectObject.send(return_data)
-        print('已经将数据总体内容发给客户端')
-
+if __name__ == '__main__':
+    s = serverSocket()
+    s.Upload()
     
 
 
