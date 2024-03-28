9d4461d73f5ce948a390ee4daced1b4a82f94989
xingzhaonian <707547168@qq.com>
2024-03-28 23:10:17 +0800

    bug fix

--- python_websocket/async_websocket_client.py
+++ python_websocket/async_websocket_client.py
@@ -1,13 +1,16 @@
 import asyncio
 import websockets
 
-async def main():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        message = "Hello, server!"
-        await websocket.send(message)
-        print(f"Sent: {message}")
+async def client():
+    async with websockets.connect("ws://127.0.0.1:8765") as websocket:
+        while True:
+            message = input('请输入要发送的消息>>>')
+            if message == 'exit':
+                break
+            await websocket.send(message)
+            print(f"Sent: {message}")
 
-        response = await websocket.recv()
-        print(f"Received: {response}")
+            response = await websocket.recv()
+            print(f"Received: {response}")
 
-asyncio.run(main())
+asyncio.run(client())
--- python_websocket/async_websocket_server.py
+++ python_websocket/async_websocket_server.py
@@ -1,13 +1,14 @@
 import asyncio
 import websockets
 
-async def echo(websocket, path):
+async def server(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
-        await websocket.send(f"Echo: {message}")
+        await websocket.send(f"收到客户端发来的消息: {message}")
 
-start_server = websockets.serve(echo, "localhost", 8765)
+start_server = websockets.serve(server, "127.0.0.1", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
+print('服务器启动完成>>>')
 asyncio.get_event_loop().run_forever()
 
