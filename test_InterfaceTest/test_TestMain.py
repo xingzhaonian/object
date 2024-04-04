9125c0df107d2954ea479f9793d62082380fdb39
xingzhaonian <707547168@qq.com>
2024-04-04 19:04:00 +0800

    注释修改

--- test_InterfaceTest/test_TestMain.py
+++ test_InterfaceTest/test_TestMain.py
@@ -10,7 +10,7 @@ import load_test_case_data.yaml_load
 
 SendMain = sync_websocket_client.ClientMain()
 
-# 主线程中启动子线程
+# 开线程
 recv_data_thread = threading.Thread(target=SendMain.recv_data)
 recv_data_thread.start()
 
