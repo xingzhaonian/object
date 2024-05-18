c046cd8f324b61c6a2731f5e44a06bd932ab0569
xingzhaonian <707547168@qq.com>
2024-05-18 14:58:44 +0800

    download music

--- Python_tools/down_load_music.py
+++ Python_tools/down_load_music.py
@@ -1,11 +1,46 @@
+import lxml
+import lxml.etree
 import requests
-url = 'https://dl.stream.qqmusic.qq.com/C400000y39oA2VspA4.m4a?guid=8158688256&vkey=8616A5F3E5201A5554FB1DE1E8ABCA782FCF62E5FFA13B598A302BB30D83BFC939D6FCB882EE7CCB31857024654EF8230E3EE5E1EAC8A9F5&uin=&fromtag=120032'
-def down_load_music(url):
-    headers_info = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44'}
-    res = requests.get(url, headers = headers_info, timeout = 30)
-    print('请求结果', type(res), res)
-    data = res.content
-    print(data, '类型:', type(data))
-    music = open('D:\\Project\\music\\可能-程响.mp3', 'wb')
-    music.write(data)
-down_load_music(url)
+
+
+def down_load_music():
+    header = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0'
+    }
+
+    singer_url = 'https://music.163.com/artist?id=2116'
+    # 请求歌手歌单页面
+    respones = requests.get(url=singer_url, headers=header)
+
+    count = 0
+    # 筛选音乐标签数据
+    doc = lxml.etree.HTML(respones.text)
+    music_list = (doc.xpath('//a[contains(@href,"/song?")]'))
+    for i in music_list:
+        href = (i.xpath('./@href')[0])
+        music_id = href.split('=')[1]
+        music_name = i.xpath('./text()')
+        
+        if music_name:
+            if '!' in music_name[0] or '{' in music_name[0] or '$' in music_name[0]:
+                continue
+            else:
+                count += 1
+                print(music_name[0] + music_id, count)
+                
+                music_url = 'http://music.163.com/song/media/outer/url?id=' + music_id
+                print(music_url)
+                music = requests.get(music_url, header)
+                try:
+                    with open('D:\\music\\' + music_name[0] + '.mp3', 'wb') as f:
+                        f.write(music.content)
+                        print(music.content)
+                except:
+                    continue
+
+down_load_music()
+
+
+
+
+
