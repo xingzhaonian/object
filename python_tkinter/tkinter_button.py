5a723e4394bcffc92b81568fac1b3dc82313d947
xingzhaonian <707547168@qq.com>
2024-05-31 09:41:57 +0800

    tkinter 设置按钮

---
+++ python_tkinter/tkinter_button.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:xingzhaonian
+
+import tkinter
+
+window = tkinter.Tk()   # 创建窗口
+window.title('测试窗口')  # 添加title
+window.geometry('500x300+700+350')  # 设置窗口大小
+
+tag_var = tkinter.StringVar()
+tag_var.set('点击按钮，进行模式切换')
+label = tkinter.Label(window, textvariable=tag_var, bg='red', font=('SimHei', 20))  # 创建标签
+label.pack()  # 放置标签
+
+
+
+is_clieck = None
+but_var = tkinter.StringVar()
+but_var.set('切换为y模式')
+def on_clieck():
+    global is_clieck
+    if is_clieck:
+        tag_var.set('当前为x模式')
+        is_clieck = False
+        but_var.set('切换y模式')
+    else:
+        tag_var.set('当前为y模式')
+        is_clieck = True
+        but_var.set('切换x模式')
+
+
+but = tkinter.Button(window, textvariable=but_var,  font=('SimHei', 15), width=15, height=1, command=on_clieck)
+but.pack()
+
+
+
+window.mainloop()
