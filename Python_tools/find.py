d558975b874073fb760b9b81d00c3512dad817ee
xingzhaonian <707547168@qq.com>
2024-08-22 10:34:05 +0800

    bug fix
    bug fix

--- Python_tools/find.py
+++ Python_tools/find.py
@@ -3,7 +3,7 @@ b = [1,2,1,3,4,1,2,3,1,2,3,4,5,67,78,89,9,2,3,4,6,8,9,0,1,1,1,1,111,1,1,2,12,]
 
 
 class list_collection:
-    def merge(param1, param2):
+    def merge(self, param1, param2):
         if len(param1) == 0:
             print('无数据')
             param1.append(param2[0])
@@ -23,19 +23,19 @@ class list_collection:
                         param1.append(k)
                         
                     
-    def merge_1(param1,param2):
+    def merge_1(self, param1,param2):
         for i in param2:
             if i not in param1:
                 param1.append(i)
 
 
 
-    def duplicat(param1, param2):
+    def duplicat(self, param1, param2):
         for i in param2:
             if i == param1:
                 return True
 
-    def merge_2(param1, param2):
+    def merge_2(self, param1, param2):
         for i in param2:
             if  not list_collection.duplicat(i, param1):
                 param1.append(i)
@@ -52,4 +52,4 @@ def loading_time(num):
         time.sleep(0.5)
         os.system("cls")
 
-loading_time(5)
+loading_time(10)
